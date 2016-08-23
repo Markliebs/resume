11742779942e9bd9e32eb2edaad9af5fe4d9a889
markliebs <markliebs@me.com>
2016-08-22 21:27:57 -0400

    Simplified pulse code

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,98 +1,12 @@
-// $(document).ready(function() {
-//     $('#second, #third').hover(function(){
-//         $('#first').addClass('blue');
-//     },
-//     function(){
-//         $('#first').removeClass('blue');
-//     });
-// });
-
-
-
-
-$(document).ready(function() {
-$( ".resPanel" ).hover(
-  function() {
-    $( this ).addClass( "animated infinite pulse" );
-    console.log("Wax On");
-  }, function() {
-    $( this ).removeClass( "animated infinite pulse" );
-    console.log("Wax Off");
-  }
-);
+$(document).ready(function () {
+  $(".resPanel, .nav1").hover(
+    function () {
+      $(this).addClass("animated infinite pulse");
+      console.log("Wax On");
+    }, function () {
+      $(this).removeClass("animated infinite pulse");
+      console.log("Wax Off");
+    }
+  );
 });
 
-$(document).ready(function() {
-$( ".nav1" ).hover(
-  function() {
-    $( this ).addClass( "animated infinite pulse" );
-    console.log("Wax On");
-  }, function() {
-    $( this ).removeClass( "animated infinite pulse" );
-    console.log("Wax Off");
-  }
-);
-});
-
-$(document).ready(function() {
-$( ".nav2" ).hover(
-  function() {
-    $( this ).addClass( "animated infinite pulse" );
-    console.log("Wax On");
-  }, function() {
-    $( this ).removeClass( "animated infinite pulse" );
-    console.log("Wax Off");
-  }
-);
-});
-
-$(document).ready(function() {
-$( ".nav3" ).hover(
-  function() {
-    $( this ).addClass( "animated infinite pulse" );
-    console.log("Wax On");
-  }, function() {
-    $( this ).removeClass( "animated infinite pulse" );
-    console.log("Wax Off");
-  }
-);
-});
-
-$(document).ready(function() {
-$( ".nav4" ).hover(
-  function() {
-    $( this ).addClass( "animated infinite pulse" );
-    console.log("Wax On");
-  }, function() {
-    $( this ).removeClass( "animated infinite pulse" );
-    console.log("Wax Off");
-  }
-);
-});
-
-
-
-// $('#resPanel').hover(
-//        function(){ $(this).addClass('animated pulse') },
-//        function(){ $(this).removeClass('animated pulse') }
-// )
-
-// $("#resPanel").hover(
-// function(){$(this).toggleClass("animated pulse" );}
-// );
-
-
-
-// $("navbar").hover(function(){
-//     $(this).css("background-color", "yellow");
-//     }, function(){
-//     $(this).css("background-color", "pink");
-// });
-
-// $( "resPanel" ).hover(
-//   function() {
-//     $( this ).addClass( "resPanelTest" );
-//   }, function() {
-//     $( this ).removeClass( "resPanelTest" );
-//   }
-// );
